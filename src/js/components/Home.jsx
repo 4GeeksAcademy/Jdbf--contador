dd8ebc831c0bde4ab71506f4a131d2b71b3a0866
Juan Diego Baron Faura <JDBF1011@Gmail.com>
2025-05-25 18:05:47 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,25 @@
-import React from "react";
+import React, from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState, useEffect} from "react";
 
-//create your first component
-const Home = () => {
+const Home =({ initialValue}) => {
+	const [Segundos, setSegundos] = useState(
+		initialValue !== undefined ? initialValue : 0
+
+	);
+
+	useEffect(() => {
+		const interval= setInterval(() =>{
+			setSegundos((counter) => counter + 1);
+		}, 500);
+	return () => clearInterval(interval);
+	}, []);
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center  ">Contador simple</h1>
+			<p>{Segundos}</p>
 		</div>
-	);
-};
+	)
+}
 
 export default Home;
